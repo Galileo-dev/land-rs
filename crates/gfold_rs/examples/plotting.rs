81f683fbdf889e3d59ef69b56858dc6e2224db29
Galileo-dev <32818066+Galileo-dev@users.noreply.github.com>
2025-04-28 11:17:55 +0100

    refactor: update plotting functions

--- crates/gfold_rs/examples/plotting.rs
+++ crates/gfold_rs/examples/plotting.rs
@@ -13,14 +13,11 @@ fn main() {
 
     plot_trajectory_3d("trajectory_chart.png", "U-E-N Trajectory", &sol).unwrap();
 
-    plot_position_velocity_time("pos_vel_chart.png", &sol, settings.simulation_settings()).unwrap();
+    plot_position_velocity_time("pos_vel_chart.png", &sol).unwrap();
 
-    plot_thrust_mass_time(
-        "thrust_mass_chart.png",
-        &sol,
-        settings.simulation_settings(),
-    )
-    .unwrap();
+    plot_thrust_time("thrust_chart.png", &sol, settings.simulation_settings()).unwrap();
+
+    plot_mass_time("mass_chart.png", &sol, settings.simulation_settings()).unwrap();
 
     plot_convergence("convergence_chart.png", &hist).unwrap();
 
